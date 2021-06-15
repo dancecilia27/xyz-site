d220b041e705c63cc4856aa6f268f652390e2aeb
Andres <Serron>
2021-06-14 23:15:35 -0300

    added blog cmpt skeleton

---
+++ pages/blog.js
@@ -0,0 +1,15 @@
+import React from "react";
+import Navbar from "./componets/navbar";
+import Footer from "./componets/footer";
+
+const Blog = () => {
+    return (
+        <div>
+            <Navbar />
+            <main><h1>Blog</h1></main>
+            <Footer />
+        </div>
+    )
+}
+
+export default Blog;
